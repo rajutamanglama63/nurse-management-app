d7197946bbd776d9035d70449395cdd7c4cb820c
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-12-10 14:42:32 +0545

    feat: connected to DB

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const { connectDB } = require("./utils/config");
 
 const app = express();
 
 app.use(express.json());
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("hello");
--- server/utils/config.js
+++ server/utils/config.js
@@ -1,9 +1,23 @@
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+const logger = require("./logger");
 
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 
+const connectDB = async () => {
+  try {
+    mongoose.connect(process.env.MONGO_URI);
+    logger.info("MongoDB connection established...");
+  } catch (error) {
+    logger.error(error);
+    process.exit(1);
+  }
+};
+
 module.exports = {
   PORT,
+  connectDB,
 };
