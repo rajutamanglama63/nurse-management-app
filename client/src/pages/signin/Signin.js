7a7298eae31b18bd742b0ca9828befef6a1ebd64
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-12-14 00:19:12 +0545

    feat: auth page designed

--- client/src/pages/signin/Signin.js
+++ client/src/pages/signin/Signin.js
@@ -1,9 +1,34 @@
-import "./signin.scss";
+import { Link } from "react-router-dom";
 
 const Signin = () => {
   return (
     <div className="signin">
-      <h1>This is signin page</h1>
+      <div className="wrapper flex block-view">
+        <div className="container container-sm">
+          <div className="flex block-view region-sm">
+            <h4 className="h6">Login</h4>
+            <p className="one-font-size">
+              Please fill the following form to login.
+            </p>
+          </div>
+          <form>
+            <input
+              className="input-field region-margin-tn border-line"
+              placeholder="Email"
+            />
+            <input
+              className="input-field region-margin-tn border-line"
+              placeholder="Password"
+            />
+            <button className="secondary-button region-margin-tn" type="submit">
+              signin
+            </button>
+            <p className="region-margin-tn">
+              Not registered yet? <Link to="/signup">Register</Link>
+            </p>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
--- client/src/pages/signup/Signup.js
+++ client/src/pages/signup/Signup.js
@@ -1,9 +1,33 @@
-import "./signup.scss";
-
+import { Link } from "react-router-dom";
 const Signup = () => {
   return (
-    <div className="signup">
-      <h1>This is signup page</h1>
+    <div className="wrapper flex block-view">
+      <div className="container container-sm">
+        <div className="flex block-view region-sm">
+          <h4 className="h6">Register</h4>
+          <p className="one-font-size">Please registered yourself.</p>
+        </div>
+        <form>
+          <input
+            className="input-field region-margin-tn border-line"
+            placeholder="Full Name"
+          />
+          <input
+            className="input-field region-margin-tn border-line"
+            placeholder="Email"
+          />
+          <input
+            className="input-field region-margin-tn border-line"
+            placeholder="Password"
+          />
+          <button className="secondary-button region-margin-tn" type="submit">
+            Signup
+          </button>
+          <p className="region-margin-tn">
+            Aready registered? <Link to="/signin">Login</Link>
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
