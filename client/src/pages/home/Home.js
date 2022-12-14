8406dab261cb652cbd4582123dd618d96af29d08
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-12-14 12:26:42 +0545

    feat: landing page designed

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,8 +1,18 @@
+import nurse from "../../images/nurse.svg";
+
 const Home = () => {
   return (
-    <div>
-      <p className="h1">Home page</p>
-      <button className="btn">ok</button>
+    <div className="wrapper region-md">
+      <div className="flex split-pair align-center split-pair">
+        <div className="flex block-view ">
+          <h1 className="h3">Nursing Management</h1>
+          <h5 className="h5">Welcome to nurse management app</h5>
+          <button className="learn-btn">Learn more</button>
+        </div>
+        <div className="flex align-center split-center">
+          <img src={nurse} alt="nursemgmt" />
+        </div>
+      </div>
     </div>
   );
 };
