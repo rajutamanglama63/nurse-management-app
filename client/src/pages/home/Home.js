df50dac4cb5614ff35c59f58c15423427d374712
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-12-14 12:32:09 +0545

    feat: made landing page mob friendly

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -3,13 +3,13 @@ import nurse from "../../images/nurse.svg";
 const Home = () => {
   return (
     <div className="wrapper region-md">
-      <div className="flex split-pair align-center split-pair">
+      <div className="flex split-pair align-center split-pair flow-direction-tn ">
         <div className="flex block-view ">
           <h1 className="h3">Nursing Management</h1>
           <h5 className="h5">Welcome to nurse management app</h5>
           <button className="learn-btn">Learn more</button>
         </div>
-        <div className="flex align-center split-center">
+        <div className="flex align-center split-center top-margin">
           <img src={nurse} alt="nursemgmt" />
         </div>
       </div>
