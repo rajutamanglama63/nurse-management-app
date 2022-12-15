fe673636d5d2ed5e6821213cc685275df6dd7c08
rajutamanglama63 <rajutamanglama63@gmail.com>
2022-12-15 10:43:34 +0545

    feat: redux store build

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import store from "./store";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+
 import App from "./App";
 import "./styles/main.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Router>
-    <App />
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 );
